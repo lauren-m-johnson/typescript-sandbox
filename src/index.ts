ad68f8891af3b244401bbee6a75882106401607f
lauren-m-johnson <laurenmj90@gmail.com>
2023-09-13 16:13:17 -0700

    update funcitons

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
 
-function calculateTax(income: number): number {
-    return 0;
+function calculateTax(income: number, taxYear: number): number {
+    if (income < 50_000)
+        return income * 1.2;
+    return income * 1.3;
 }
+
+calculateTax(10_000, 2022, 1)
